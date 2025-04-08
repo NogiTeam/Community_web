4af534db892e71b325770c31a7a3002bdf462b65
NogiTeam <akunnyasoeltan3@gmail.com>
2025-04-09 00:57:02 +0700

    Update ScrollText.tsx

--- src/components/homepage/ScrollText.tsx
+++ src/components/homepage/ScrollText.tsx
@@ -4,7 +4,7 @@ function ScrollText() {
    return (
       <div className="md:py-10">
          <VelocityScroll
-            text="!NIGHTCODER COMMUNITY"
+            text="FOREX CURRENCY"
             default_velocity={2}
             className="font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm text-white/20 md:text-7xl md:leading-[5rem] mdLh-24"
          />
